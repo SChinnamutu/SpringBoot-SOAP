18a7e75f5ec2d40f03224ea6c947c5ffce205220
Sasi <mech.sasi82@gmail.com>
2020-04-05 00:34:22 +0530

    Code optimized

--- src/main/java/com/tfg/keybank/ws/service/UserProfileService.java
+++ src/main/java/com/tfg/keybank/ws/service/UserProfileService.java
@@ -1,6 +1,6 @@
 package com.tfg.keybank.ws.service;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.keybank.tfa.v1_11.service.operations.ListUserResult;
@@ -9,16 +9,19 @@ import com.tfg.keybank.ws.config.SoapClinetConfig;
 
 @Service
 public class UserProfileService {
+	
+	@Autowired
+	SoapClinetConfig config;
 
-	@SuppressWarnings("resource")
+	@Autowired
+	UserListClient client;
+	
 	public ListUserResult processFetchUserName(String userId) {
-		AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(SoapClinetConfig.class);
-		UserListClient clinet = annotationConfigApplicationContext.getBean(UserListClient.class);
 		try {
 			System.out.println("============Api Invocation Begins===========================");
-			System.out.println("Result :: " + clinet.processfetchUsers(userId).toString());
+			System.out.println("Result :: " + client.processfetchUsers(userId).toString());
 			System.out.println("============Api Invocation Ends===========================");
-			return clinet.processfetchUsers(userId);
+			return client.processfetchUsers(userId);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
